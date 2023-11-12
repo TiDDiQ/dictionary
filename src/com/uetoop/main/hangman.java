30791ba6fc3e26207b52daa823e50575f4f884f0
Tra Nguyen <147636502+wannacodewme@users.noreply.github.com>
2023-11-12 21:12:06 +0700

    Create hangman.java

---
+++ src/com/uetoop/main/hangman.java
@@ -0,0 +1,66 @@
+package com.uetoop.main;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Hangman {
+
+    public static void main(String[] args) {
+        Random obj = new Random();
+        int Ran_num = obj.nextInt(108000);
+        String random = Integer.toString(Ran_num);
+        DictionaryDatabase db = new DictionaryDatabase();
+        String word = db.findWord("id", random);
+        Scanner sc = new Scanner(System.in);
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int remainingGuesses = 5;
+            StringBuilder guessedLetters = new StringBuilder();
+            StringBuilder wordInProgress = new StringBuilder(word.length());
+
+            for (int i = 0; i < word.length(); i++) {
+                wordInProgress.append("_");
+            }
+
+            while (remainingGuesses > 0 && wordInProgress.indexOf("_") != -1) {
+                System.out.println("Word: " + wordInProgress.toString());
+                System.out.println("Guessed letters: " + guessedLetters.toString());
+                System.out.println("Remaining guesses: " + remainingGuesses);
+                System.out.print("Guess a letter: ");
+                String guess = sc.nextLine().toLowerCase();
+
+                if (guessedLetters.indexOf(guess) != -1) {
+                    System.out.println("You already guessed that letter!");
+                    continue;
+                }
+
+                guessedLetters.append(guess);
+
+                if (word.indexOf(guess) != -1) {
+                    for (int i = 0; i < word.length(); i++) {
+                        if (word.charAt(i) == guess.charAt(0)) {
+                            wordInProgress.setCharAt(i, guess.charAt(0));
+                        }
+                    }
+                } else {
+                    remainingGuesses--;
+                    System.out.println(guess.toUpperCase() + " is not in the word.");
+                }
+            }
+
+            if (remainingGuesses == 0) {
+                System.out.println("You lose! The word was " + word.toUpperCase() + ".");
+            } else {
+                System.out.println("You win! The word was " + word.toUpperCase() + ".");
+            }
+
+            System.out.print("Play again? (y/n): ");
+            String playAgainInput = sc.nextLine().toLowerCase();
+
+            if (!playAgainInput.equals("y")) {
+                playAgain = false;
+            }
+        }
+        sc.close();
+    }
+}
