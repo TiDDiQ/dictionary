b8cb46c5200f501643a743359f87b02166a79ebf
ThuHuyen-Vu2425 <huyenthuvu25@gmail.com>
2023-11-14 15:50:08 +0700

    xóa version2

--- src/com/uetoop/main/HelloApplication.java
+++ src/com/uetoop/main/HelloApplication.java
@@ -1,7 +1,8 @@
-package com.uetoop.main;
+package demo.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,11 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 400, 350);
+        stage.setTitle("Dictionary");
         stage.setScene(scene);
         stage.show();
     }
+
+    //public String
 
     public static void main(String[] args) {
         launch();
