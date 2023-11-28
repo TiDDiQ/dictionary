469ab69bdb9b53bc94031fd64c93fe89a94bca76
Tra Nguyen <147636502+wannacodewme@users.noreply.github.com>
2023-11-28 10:36:37 +0700

    Add files via upload

---
+++ src/com/uetoop/main/Main.java
@@ -0,0 +1,25 @@
+// hangman game
+package com.uetoop.main;
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sampleHangman.fxml"));
+        primaryStage.setTitle("Hangman");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
