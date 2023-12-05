de1ec842586867514458b2bd0c49dc7d66e0490d
TiDDiQ2 <151905645+TiDDiQ2@users.noreply.github.com>
2023-12-05 10:32:14 +0700

    Update googleController.java

--- src/com/uetoop/main/Dic_main/googleController.java
+++ src/com/uetoop/main/Dic_main/googleController.java
@@ -1,5 +1,7 @@
 package com.uetoop.main.Dic_main;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import com.uetoop.main.Google;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -18,26 +20,20 @@ public class googleController {
     private AnchorPane gg;
 
     @FXML
-    private Label resultText;
+    private TextField resultText;
+
+    @FXML
+    private Button gtranslateButton;
 
     private Google google = new Google();
 
-
     @FXML
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Assuming you want to trigger translation when the application starts
+    void googleTranslateInit(ActionEvent event) {
         String currentWord = inputText.getText();
-
         try {
-            String meaning = setText(google.translate(currentWord));
+            resultText.setText(google.translate(currentWord));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    // Assuming you have a method to set the text in your resultText
-    private String setText(String translation) {
-        resultText.setText(translation);
-        return translation;
     }
 }
